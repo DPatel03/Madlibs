69934bafc771bc46802ff02ded9195fbea2aded4
Deep200 <deep.patel.0603@gmail.com>
2022-04-29 14:16:33 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous Person: ")
+
+madlib = f" Computer programing is so {adj}!! It has makes me so excited all time because \
+I love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!!"
+
+print(madlib)
